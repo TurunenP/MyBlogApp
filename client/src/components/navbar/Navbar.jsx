5711ee4e9ca1c30cd2921d310e31312fe9f9f4a5
TurunenP <k7jepu00@students.oamk.fi>
2025-07-21 17:37:37 +0300

    Updated footer and navbar components

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import classes from './navbar.module.css'
-import { Link } from 'react-router-dom'
-import womanImg from '../../assets/woman.jpg'
-import { useState } from 'react'
+import React from "react";
+import classes from "./navbar.module.css";
+import { Link } from "react-router-dom";
+import womanImg from "../../assets/woman.jpg";
+import { useState } from "react";
 
 const Navbar = () => {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
         <div className={classes.left}>
-          <Link to='/'>WebDevMania</Link>
+          <Link to="/">MyBlogApp</Link>
         </div>
         <ul className={classes.center}>
           <li className={classes.listItem}>Home</li>
@@ -20,17 +20,21 @@ const Navbar = () => {
           <li className={classes.listItem}>Categories</li>
         </ul>
         <div className={classes.right}>
-          <img onClick={() => setShowModal(prev => !prev)} src={womanImg} className={classes.img} />
-          {showModal &&
+          <img
+            onClick={() => setShowModal((prev) => !prev)}
+            src={womanImg}
+            className={classes.img}
+          />
+          {showModal && (
             <div className={classes.modal}>
-              <Link to='/create'>Create</Link>
+              <Link to="/create">Create</Link>
               <span>Logout</span>
             </div>
-          }
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
